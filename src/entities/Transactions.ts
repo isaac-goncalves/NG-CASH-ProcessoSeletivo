7d10787d7ac3d7ec87b22e28d5add9c6ec115864
Isaac Correia Gonçalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-11-17 19:27:22 -0300

    -

--- src/entities/Transactions.ts
+++ src/entities/Transactions.ts
@@ -1,5 +1,5 @@
 
-import { Entity, PrimaryGeneratedColumn, Column,  ManyToOne } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column,  ManyToOne, CreateDateColumn, JoinColumn} from "typeorm"
 
 import { Accounts } from "./Accounts"
 
@@ -9,11 +9,16 @@ export class Transactions {
     id: number
 
     @ManyToOne(() => Accounts, account => account.id)  
+    @JoinColumn()
     debitedAccountid: Accounts
 
     @ManyToOne(() => Accounts, account => account.id)
-    creditedAccountid: number
+    @JoinColumn()
+    creditedAccountid: Accounts
 
     @Column()
     value: number
+
+    @CreateDateColumn()
+    created_at: Date;
 }
