4cc22d10623c2ebfec883a2c18a8597653809d80
Isaac Correia Gonçalves <isaac.correia.2406@gmail.com>
2023-02-13 18:54:47 -0300

    adding repository in new way

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,12 +6,12 @@ import { DataSource } from "typeorm";
 const port = process.env.DB_PORT as number | undefined;
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST,
     port: port,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    type: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "postgres",
+    database: "rentx",
     entities:   [`${__dirname}/**/entities/*.{ts,js}`],
     migrations:   [`${__dirname}/**/migrations/*.{ts,js}`],
 })
